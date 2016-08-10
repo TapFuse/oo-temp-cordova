83fe28984fea96e1201ed609da76ddd6266d73b5
Lauricio Su <me@lauricio.com>
2016-08-10 13:47:02 +0700

    use webview 2.0.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ox2:temp-cordova',
-  version: '2.0.0',
+  version: '3.0.0',
   // Brief, one-line summary of the package.
   summary: 'DO NOT USE',
   // URL to the Git repository containing the source code for this package.
@@ -28,7 +28,7 @@ Package.onUse(function(api) {
 });
 
 Cordova.depends({
-  "cordova-plugin-crosswalk-webview": "1.8.0",
+  "cordova-plugin-crosswalk-webview": "2.0.0",
   "phonegap-plugin-push": "1.5.3",
   "cordova-plugin-device": "1.1.2",
   "cordova-plugin-inappbrowser": "1.4.0",
