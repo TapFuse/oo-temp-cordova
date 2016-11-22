d8d94b2ec3b46be92709239a7a760aff045eea4f
Pauli B <pauli.buccini@gmail.com>
2016-11-22 19:23:23 +0700

    updated dependencies

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ox2:temp-cordova',
-  version: '3.1.0',
+  version: '4.0.0',
   // Brief, one-line summary of the package.
   summary: 'DO NOT USE',
   // URL to the Git repository containing the source code for this package.
@@ -15,7 +15,7 @@ var S = 'server';
 var CS = [C, S];
 
 Package.onUse(function(api) {
-    api.versionsFrom('1.2.1');
+    api.versionsFrom('1.4.2');
     // Core
     api.use([
       'ecmascript'
@@ -28,14 +28,13 @@ Package.onUse(function(api) {
 });
 
 Cordova.depends({
-  "cordova-plugin-crosswalk-webview": "2.0.0",
-  "phonegap-plugin-push": "1.5.3",
-  "cordova-plugin-device": "1.1.2",
-  "cordova-plugin-inappbrowser": "1.4.0",
-  "cordova-plugin-statusbar": "2.1.3",
-  "cordova-plugin-splashscreen": "3.2.2",
-  "cordova-plugin-camera": "2.2.0",
-  "org.madmobile.cordovaplugins.zbarscanner": "1.0.1",
+  "cordova-plugin-crosswalk-webview": "2.1.0",
+  "phonegap-plugin-push": "1.9.0",
+  "cordova-plugin-device": "1.1.3",
+  "cordova-plugin-inappbrowser": "1.5.0",
+  "cordova-plugin-statusbar": "2.2.0",
+  "cordova-plugin-splashscreen": "4.0.0",
+  "cordova-plugin-camera": "2.3.0",
 });
 
 Package.onTest(function(api) {
