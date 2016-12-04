24c1d39dad7e571a3d57b3396a693851892921c3
Pauli B <pauli.buccini@gmail.com>
2016-12-04 17:15:19 +0700

    version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ox2:temp-cordova',
-  version: '4.0.0',
+  version: '4.0.4',
   // Brief, one-line summary of the package.
   summary: 'DO NOT USE',
   // URL to the Git repository containing the source code for this package.
@@ -15,7 +15,7 @@ var S = 'server';
 var CS = [C, S];
 
 Package.onUse(function(api) {
-    api.versionsFrom('1.4.2');
+    api.versionsFrom('1.4.1');
     // Core
     api.use([
       'ecmascript'
@@ -29,12 +29,14 @@ Package.onUse(function(api) {
 
 Cordova.depends({
   "cordova-plugin-crosswalk-webview": "2.1.0",
-  "phonegap-plugin-push": "1.9.0",
+  "phonegap-plugin-push": "1.5.3",
   "cordova-plugin-device": "1.1.3",
+  "cordova-plugin-safariviewcontroller": "1.4.6",
   "cordova-plugin-inappbrowser": "1.5.0",
   "cordova-plugin-statusbar": "2.2.0",
   "cordova-plugin-splashscreen": "4.0.0",
   "cordova-plugin-camera": "2.3.0",
+  "cordova-plugin-customurlscheme": "4.2.0",
 });
 
 Package.onTest(function(api) {
