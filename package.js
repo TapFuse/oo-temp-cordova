b84f6eb0e62861689ef11e5bd98a4ff8cdc94bbd
Po Buchi <po@ox2.io>
2017-03-15 14:18:34 +0700

    v5.0.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ox2:temp-cordova',
-  version: '4.2.0',
+  version: '5.0.0',
   // Brief, one-line summary of the package.
   summary: 'DO NOT USE',
   // URL to the Git repository containing the source code for this package.
@@ -28,10 +28,8 @@ Package.onUse(function(api) {
 });
 
 Cordova.depends({
-  "cordova-plugin-crosswalk-webview": "2.3.0",
   "phonegap-plugin-push": "1.9.0",
   "cordova-plugin-device": "1.1.4",
-  "cordova-plugin-safariviewcontroller": "1.4.6",
   "cordova-plugin-inappbrowser": "1.6.1",
   "cordova-plugin-statusbar": "2.2.1",
   "cordova-plugin-splashscreen": "4.0.1",
