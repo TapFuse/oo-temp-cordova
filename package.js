2e674d52e6295f6e495631b78b8417c053405b08
Po Buchi <po@ox2.io>
2017-03-15 14:17:32 +0700

    initial

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
-  name: 'ox2:temp-cordova',
-  version: '4.2.0',
+  name: 'ox2:temp-cordova-android',
+  version: '1.0.0',
   // Brief, one-line summary of the package.
   summary: 'DO NOT USE',
   // URL to the Git repository containing the source code for this package.
@@ -20,28 +20,9 @@ Package.onUse(function(api) {
     api.use([
       'ecmascript'
     ]);
-    // 3rd party
-    api.use([
-
-    ]);
-    api.addFiles('lib/oo-temp-cordova.js', C);
 });
 
 Cordova.depends({
   "cordova-plugin-crosswalk-webview": "2.3.0",
-  "phonegap-plugin-push": "1.9.0",
-  "cordova-plugin-device": "1.1.4",
-  "cordova-plugin-safariviewcontroller": "1.4.6",
-  "cordova-plugin-inappbrowser": "1.6.1",
-  "cordova-plugin-statusbar": "2.2.1",
-  "cordova-plugin-splashscreen": "4.0.1",
-  "cordova-plugin-camera": "2.3.1",
-  "cordova-plugin-customurlscheme": "4.2.0",
 });
 
-Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('ox2:temp-cordova');
-  api.addFiles('tests/package-tests.js');
-});
